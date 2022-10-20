186f698a87bb3b554800715d2675273b1c2494f5
Lucas Sulzbach rilho <lsulzbac@car10s2.42barcelona.com>
2022-10-20 13:25:52 +0200

    bonus part

---
+++ get_next_line_bonus.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsulzbac <lsulzbac@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/18 11:57:25 by lsulzbac          #+#    #+#             */
+/*   Updated: 2022/10/20 13:22:25 by lsulzbac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+void	*clean_buf(t_gnl *gnl)
+{
+	gnl->buf_size = 0;
+	if (gnl->buf != NULL)
+	{
+		free(gnl->buf);
+		gnl->buf = NULL;
+	}
+	return (NULL);
+}
+
+char	*fuck_lines(t_gnl *gnl, int read_result)
+{
+	if (read_result < 0)
+		return (clean_buf(gnl));
+	return (final_buf(gnl));
+}
+
+char	*read_from_file(t_gnl *gnl, int fd)
+{
+	char	*tmp;
+	int		read_size;
+
+	tmp = (char *) malloc (BUFFER_SIZE + 1);
+	if (tmp == NULL)
+		return (clean_buf(gnl));
+	tmp[0] = '\0';
+	while (has_new_line(tmp) < 0)
+	{
+		read_size = read (fd, tmp, BUFFER_SIZE);
+		if (read_size <= 0)
+		{
+			free(tmp);
+			return (fuck_lines(gnl, read_size));
+		}
+		tmp[read_size] = '\0';
+		if (ft_join(gnl, tmp, read_size))
+		{
+			free(tmp);
+			return (clean_buf(gnl));
+		}
+	}
+	free(tmp);
+	return (get_string(gnl, fd));
+}
+
+char	*get_string(t_gnl *gnl, int fd)
+{
+	int		line;
+
+	line = 0;
+	if (gnl->buf != NULL)
+	{
+		line = has_new_line(gnl->buf);
+		if (line >= 0)
+		{
+			if (line + 1 == gnl->buf_size)
+				return (final_buf(gnl));
+			return (reset_buf(gnl, line));
+		}
+	}
+	return (read_from_file(gnl, fd));
+}
+
+char	*get_next_line(int fd)
+{
+	static t_gnl	gnl[1024];
+	char			*result;
+
+	result = NULL;
+	if (fd >= 0 && BUFFER_SIZE > 0)
+		result = get_string(&gnl[fd], fd);
+	return (result);
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsulzbac <lsulzbac@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/14 17:13:44 by lsulzbac          #+#    #+#             */
+/*   Updated: 2022/10/20 13:18:18 by lsulzbac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <stddef.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+typedef struct gnl_struct {
+	char	*buf;
+	int		buf_size;
+}	t_gnl;
+
+int		has_new_line(char *str);
+char	*final_buf(t_gnl *gnl);
+char	*reset_buf(t_gnl *gnl, int new_line);
+char	*get_next_line(int fd);
+char	*get_string(t_gnl *gnl, int fd);
+char	*read_from_file(t_gnl *gnl, int fd);
+int		ft_join(t_gnl *gnl, char *str, int size);
+void	ft_strcpy(char *dst, char *src, int size);
+void	*clean_buf(t_gnl *gnl);
+
+#endif
